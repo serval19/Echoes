5b029e517f3a3a18f1b7c39c26f2e45a0d935912
serval19 <diwatomy04@gmail.com>
2025-04-26 23:00:41 +0530

    login api finished

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,4 +1,5 @@
 const express=require('express')
+require('dotenv').config()
 const router=require('express').Router()
 const UserModel=require('../Models/diary.js')
 const bcrypt=require('bcrypt')
@@ -21,5 +22,29 @@ router.post('/signup',async (req,res)=>{
     catch(err){
         res.status(500).json({message: 'internal server error',success:false})
     }
+})
+router.post('/login',async (req,res)=>{
+    const {email,password}=req.body
+    const user=await UserModel.findOne({email})
+    
+    if(!user){
+        return res.status(401).json({message: "Signup first",success:false})
+    }
+    const ispassequal=await bcrypt.compare(password,user.password)
+    if(!ispassequal){
+        return res.status(403).json({message: "Wrong Password",success:false })
+    }
+    const jwtToken=jwt.sign(
+        {email: user.email,_id: user._id},
+        process.env.JWT_SECRET,
+        {expiresIn: '24h'}
+
+    )
+    return res.status(200).json({message:'login successful',success:true,
+        jwtToken,
+        email,
+        name: user.name
+    })
+
 })
 module.exports=router
