a0653fc17721b068d30487e626274055318f6dc3
serval19 <diwatomy04@gmail.com>
2025-05-07 23:26:47 +0530

    login page top bar and side image

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import doodleart from '../assets/doodleart.jpg'
 
 function Login() {
   return (
-    <div className='text-[50px]'>L</div>
+    <>
+     <div className='bg-black'><span className='font-serif text-white pl-4 text-3xl'>Echoes</span></div>
+     <div className='max-h-screen overflow-hidden flex flex-col w-3/4'>
+      <div className='h-full w-4/5 '>
+        <img src={doodleart} alt='doodleart' className='h-full'/>
+      </div>
+      <div className='h-full  w-4/5'>
+        <img src={doodleart} alt='doodleart' className='h-full'/>
+      </div>
+
+     </div>
+    </>
+
+   
+    
   )
 }
 
