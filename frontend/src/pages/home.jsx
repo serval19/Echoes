0c9d64e728307732ac8c7de1596a7f5c55f746a7
serval19 <diwatomy04@gmail.com>
2025-05-16 17:10:37 +0530

    added spinner and use effect to fetch entries

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 import doodleart from '../assets/doodleart.jpg'
 import { successmsg } from '../components/notification'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { ClimbingBoxLoader } from "react-spinners";
+
 
 function Home() {
   const navigate=useNavigate()
+  const [loading,setLoading]=useState(false)
+  useEffect(()=>{
+      setLoading(true)
+      const token=localStorage.getItem('jwtToken')
+      axios.get('http://localhost:8080/entries',{
+        headers:{
+          'Authorization': token
+        }
+      })
+      .then((response)=>{
+        console.log(response.data.entries)
+        
+      })
+      .catch((err)=>{
+        console.log(err)
+        
+      })
+      .finally(
+        ()=>{
+          setTimeout(()=>{
+            setLoading(false)
+          },2000)
+        }
+      )
+
+    },[])
   function handleLogout(){
     localStorage.removeItem('jwtToken')
     localStorage.removeItem('name')
@@ -12,6 +42,7 @@ function Home() {
     setTimeout(()=>{
       navigate('/login')
     },1000)
+    
 
   }
   return (
@@ -20,6 +51,10 @@ function Home() {
       <span className='font-serif text-white pl-4 text-3xl'>Echoes</span>
       <span className='justify-end'><button className='bg-blue-700 text-white text-xl py-1 px-4 mt-1 mb-1 mr-3 rounded-2xl hover:bg-blue-500 font-mono' onClick={handleLogout}>Logout</button></span>
     </div>
+    <div>
+
+    </div>
+    { loading ?  <ClimbingBoxLoader color="#000000" size={20} className="mx-auto mt-20" /> : <div>Loaded</div>}
 
     
 
